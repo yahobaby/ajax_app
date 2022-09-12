e163ee5c310232b888731cba8a4272d9b111fc51
yahobaby <hellotechcamp@gmail.com>
2022-09-12 16:05:26 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,17 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+    # @posts = Post.all メモを降順、新しい順に表示される為に、編集
+    @posts = Post.order(id: "DESC") #メモを降順にする
+
   end
 
-  def new
-  end
+  # def new→newアクションは不要の為
+  # end
 
   def create
     Post.create(content: params[:content])
+    redirect_to action: :index  # 追記する
+    # 投稿後にトップページにリダイレクトする
   end
 end
