645adcc3885bd5f56dfea4907f0b693f84834b09
yahobaby <hellotechcamp@gmail.com>
2022-09-17 01:03:20 +0900

    リクエストを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,8 +10,23 @@ class PostsController < ApplicationController
   # end
 
   def create
-    Post.create(content: params[:content])
-    redirect_to action: :index  # 追記する
+    # binding.pry : createアクションの中にbinding.pryを記述することで、リクエストが送信されているかどうか確認.
+    # サーバーを立ち上げて、http://localhost:3000 にアクセスし,メモを投稿したら、処理が止まるので、ターミナルを確認して、正常にリクエストが送信されてるか
+    # 確認をする。確認できたら、削除
+
+  
+    # 新たに投稿されたメモの内容を変数postに格納
+    post = Post.create(content: params[:content])
+
+
+
+    render json:{ post: post }
+    # renderメソッドを用いて、レスポンスで返却されるデータフォーマットにJSONを指定
+    # json:の部分をjsonオプションといい、これを指定することによって、直後に記述した{ post: post }というデータをJSON形式で返却
+    # 直上で定義した変数postの値を、postというキーとセットでJavaScriptに送信
+
+    # Post.create(content: params[:content])
+    # redirect_to action: :index  # 追記する
     # 投稿後にトップページにリダイレクトする
   end
 end
